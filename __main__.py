c116dc73242be7b959d2b7e6ec78c91addb88ac6
pedritastark <sebastianpedraza1010@gmail.com>
2023-11-04 10:14:05 -0500

    Update optimization algortihms manager

--- __main__.py
+++ __main__.py
@@ -11,9 +11,13 @@ Optimizacion en una variable
 
 import matplotlib.pyplot as plt
 import numpy as np
-import sympy as sp
 
 class OptimizerManager:
+    '''
+    Descripcion: Se crea la clase donde vamos a almacenar los
+    metodos de optimizacion, ademas de los metodos para agregarlos
+    y ejecuarlos sobre la funcion objetivo
+    '''
     def __init__(self):
         self.optimizers = {}
         self.algorithm = None
@@ -28,17 +32,23 @@ class OptimizerManager:
         else:
             raise ValueError(f"El metodo '{algorithm_name}' no esta registrado")
 
-    
+
 
     ## Metodo para graficar la funcion
     def GraphObjectiveFunction(self):
-        
-        'Usamos esta funcion para graficar la funcion que estamos evaluando'
-        'a y b son los limites a los cuales esta regida la funcion [a,b]'
-        
-        a, b = -10, 10    
+
+        '''
+        Descripcion: Funcion para graficar la funcion objetivo.
+
+        Input: None
+
+        Output: Grafica (plot) -> Se muestra la grafica de la funcion obejtivo en el intervalo que la vamos a trabajar, la funcion
+        objetivo esta pre-definida en el metodo U(T), del mismo modo los limites con func_limit
+        '''
+
+        a, b = self.func_limit()[0], self.func_limit()[1]
         T = np.linspace(a, b, 100)
-        U = 3*(T**4) + 4*(T**3)
+        U = [self.U(t) for t in T]
 
         plt.figure(figsize=(6, 3))
         plt.plot(T, U, 'b')
@@ -46,30 +56,46 @@ class OptimizerManager:
         plt.ylabel("f(x)")
         plt.grid()
         plt.show()
-        
-        
-    '''    
-    U(T) es una funcion objetivo  que usamos para encontrar el minimo
-    usando los algoritmos algoritmos por  ejemplo en el algoritmo de busqueda
-    de la seccion dorada para ir reduciendo el intervalo donde se encuentra la solucion optima
-    el argumento T es sobre el cual evaluamos la funcion objetivo en ese punto (T, f(T))
-    '''
 
-    def U(self, T):
-        return sp.exp(-T) + sp.log(T**2 + 1)
 
-    def derU(self, T):
-        return 2*T/(T**2 + 1) - np.exp(-T)
+    def func_limit(self):
+        '''
+        Descripcion: Funcion definimos los limites [a, b] donde se va a trabajar la funcion objetivo
+
+        Input: None
+
+        Output: limites de integracion (list) -> Se obtiene el primer intervalo sobre el cual vamos a delimitar la funcion obejtivo
+        '''
+
+        return [-4, 4]
+
+
+
+
+    def U(self, T: float):
+
+        '''
+        Descripcion: Funcion para evaluar la funcion objetivo en un punto x, recuerde que U(T) es la funcion objetivo que usamos para encontrar el minimo
+
+        Input: T(float) ->  T que es el punto donde se va a evaluar la funcion objetivo
+
+        Output: U{T}(float) -> Se devuelve U(T) que es la funcion objetivo evaluada en el punto T
+        '''
+
+        return T**4 - T**2 -4*T
+
+    def derU(self, T: float):
+        return 4*(T**3) -2*T - 4
 
     def der2U(self, T):
-        return -4*T**2/(T**2 + 1)**2 + np.exp(-T) + 2/(T**2 + 1)
-    
+        return 36*(T**2) + 24*T
+
     ## Metodo de newton
     def newtonSearch(self):
         epsilon   = 0.0001
-        lambda_k  = -2   
+        lambda_k  = -2
         k         = 1
-        
+
         while True:
             if abs(self.derU(lambda_k)) < epsilon:
                 print("-------------------------------------------------------")
@@ -81,36 +107,32 @@ class OptimizerManager:
                     print("-------------------------------------------------------")
                     print("Punto óptimo {}".format(lambda_k_1))
                     break
-                else: 
+                else:
                     lambda_k = lambda_k_1
             k += 1
-            
 
-    ## Metodo de la seccion dorada    
+
+    ## Metodo de la seccion dorada
     def GoldenSectionSearch(self):
-        
-    
-
-    
         '''
-        Definimos  [a,b] el intervalo incial de busqueda, alpha la constante 
-        sobre la cual calculamos lambda_k y miu_k, h es la constante 
-        sobre la cual definimos el espacio entre el intervalo encontrado 
+        Funcionamiento: Sea  [a,b] el intervalo incial de busqueda, alpha la constante
+        sobre la cual calculamos lambda_k y miu_k, h es la constante
+        sobre la cual definimos el espacio entre el intervalo encontrado
         '''
-        a       =  -2
-        b       =  2
+        a       =  self.func_limit()[0]
+        b       =  self.func_limit()[1]
         alpha   =  (5**(1/2)-1)/2
         h       =  0.001
-        
+
         '''
-        el contador solo lo usamos para mostrar el numero de iteraciones que hemos realizado
+        Contador solo lo usamos para mostrar el numero de iteraciones que hemos realizado
         data guarda los valores de el numero de iteracion, lambda y f(lambda)
         '''
-        
+
         cont = 0
 
         while(True):
-            
+
             'calculamos alpha 1 y miu'
             cont    =  cont + 1
             lambda1 =  a + (1-alpha) * (b-a)
@@ -119,166 +141,167 @@ class OptimizerManager:
             'calulmamos f(alpha1) y f(alpha2)'
             U_lambda1 = self.U(lambda1)
             U_miu1 = self.U(miu1)
-            
+
             'como f(lambda) > f(miu), entonces el limite sobre el intervalo [a,b] pasa a ser'
             '[lambda, b]'
             if(U_lambda1 > U_miu1):
                 # print("It: {:02d} - a_k: {:.10f} - b_k: {:.10f}  - l_k {:.10f}  - u_k{:.10f}  - f(l_k) {:.10f}  - f(u_k) {:.10f} - b_k-a_k {:.10f}".format(cont, a, b, lambda1, miu1, U(lambda1), U(miu1), b-a))
                 a = lambda1
-                
+
                 'de otro modo el limite se reduce el intervalo [a, miu]'
             else:
                 # print("It: {:02d} - a_k: {:.10f} - b_k: {:.10f}  - l_k {:.10f}  - u_k{:.10f}  - f(l_k) {:.10f}  - f(u_k) {:.10f} - b_k-a_k {:.10f}".format(cont, a, b, lambda1, miu1, U(lambda1), U(miu1), b-a))
 
-                b = miu1       
+                b = miu1
 
             'Mostramos numero de iteracion y el intervalo al cual vamos reduciendo'
             print("It: {:02d} - a_k: {:.10f} - b_k: {:.10f}".format(cont, a, b))
 
             if(np.abs(a - b) < h):
-                
+
                 'Mostramos el intervalo final luego de que este cumpla con el maximo'
                 'de distancia entre los puntos a y b sea menos a h'
-                
+
                 print("-------------------------------------------------------")
                 print("It: {:02d} - a_k: {:.10f} - b_k: {:.10f}".format(cont, a, b))
-                
+
                 'Como la solucion optima pertenece al intervalo [a,b]'
                 'retornamos (a+b) / 2'
 
                 print("(a_k + b_k)/2 == {}".format((a + b)/2))
                 break
-            
+
 
     ## Metodo de biseccion
     def bisectionSearch(self):
         '''
-        Definimos  [a,b] el intervalo incial de busqueda,h es la constante 
+        Funcionamiento: Sea [a,b] el intervalo incial de busqueda,h es la constante
         sobre la cual definimos el espacio entre el intervalo (intervalo de incertidumbre),
         cont es el contador para mostar el numero de iteracion
         '''
-        a    = -4
-        b    = 4
-        h    = 0.0001
-        cont = 0
+        a    =  self.func_limit()[0]
+        b    =  self.func_limit()[1]
+        h    =  0.0001
+        cont =  0
         while True:
             cont += 1
-            
-            
+
+
             if b-a < h:
-                
+
                 'Mostramos el intervalo final luego de que este cumpla con el maximo'
                 'de distancia entre los puntos a y b sea menos a h'
-                
+
                 print("-------------------------------------------------------")
                 print("It: {:02d} - a_k: {:.10f} - b_k: {:.10f}".format(cont, a, b))
-                
+
                 'Como la solucion optima pertenece al intervalo [a,b]'
                 'retornamos (a+b) / 2'
                 print("(a_k + b_k)/2 == {}".format((a + b)/2))
                 break
-            
-            'Calculamos lambda_k' 
-            lambda_k = (a+b)/2 
-            
+
+            'Calculamos lambda_k'
+            lambda_k = (a+b)/2
+
             'Como el algoritmo se aplica sobre funciones pseudoconvexa'
             'si la derivada en le punto lambda_k es 0, entonces estamos en le minimo'
-            
+
             if self.derU(lambda_k) == 0:
                 print("-------------------------------------------------------")
                 print("Solucion optima == {}".format(lambda_k))
-                
-                
+
+
                 'Si la derivada es >0 evaluada en lambda_k como es una funcion pseudoconvexa'
                 'el nuevo intervalo es [a, lambda_k]'
-            elif self.derU(lambda_k) > 0: 
+            elif self.derU(lambda_k) > 0:
                 # print("It: {:02d} - a_k: {:.10f} - b_k: {:.10f} - l_k: {:.10f} - f(l_k): {:.10f} - a_k-b_k: {:.10f}".format(cont, a, b, lambda_k, derU(lambda_k), b-a))
                 b = lambda_k
-                
-                
+
+
                 'Si la derivada es <0 evaluada en lambda_k como es una funcion pseudoconvexa'
                 'el nuevo intervalo es [lambda_k, b]'
-            else: 
+            else:
                 # print("It: {:02d} - a_k: {:.10f} - b_k: {:.10f} - l_k: {:.10f} - f(l_k): {:.10f} - a_k-b_k: {:.10f}".format(cont, a, b, lambda_k, derU(lambda_k), b-a))
                 a = lambda_k
             print("It: {:02d} - a_k: {:.10f} - b_k: {:.10f}".format(cont, a, b))
-            
-    
+
+
     # Metodo de Fibonacci
-    
+
     def fibonacci(self,n):
         '''
-        Fibonnaci retorna el resultado de la sucesion de fibonacci usado en el metodo de busqueda 
-        para encontrar  el numero de iteraciones que vamos a hacer para encontrar el 
+        Funcionamiento: Fibonnaci retorna el resultado de la sucesion de fibonacci usado en el metodo de busqueda
+        para encontrar  el numero de iteraciones que vamos a hacer para encontrar el
         intervalo donde esta la  solucion optima
         '''
-    
-        if n<= 1: return 1 
+
+        if n<= 1: return 1
         else: return self.fibonacci(n-1) + self.fibonacci(n-2)
+
     def iterationFinder(self,a,b,h):
         cv = (b-a)/h
         i = 0
         while True:
             if self.fibonacci(i) > cv: return i
-            i+=1    
-        
-    
+            i+=1
+
+
     def fibonacciSearch(self):
         '''
-        Definimos  [a,b] el intervalo incial de busqueda,h la longitud final
+        Funcionamiento: Sea[a,b] el intervalo incial de busqueda,h la longitud final
         de incertidumbre y cont para llevar contador para mostar el numero de iteracion,
         e que es un epsilon > 0 que es un constante de distinguibilidad que cumple con
         e < h y n la cantidad de iteraciones
         '''
 
-        a    =  -4
-        b    =  2
+        a    =  self.func_limit()[0]
+        b    =  self.func_limit()[1]
         e    =  0.01
         h    =  0.1
-        cont =  1        
-        n    =  self.iterationFinder(a, b, h)  
-        
+        cont =  1
+        n    =  self.iterationFinder(a, b, h)
+
         while True:
-            
+
             'Calculamos lambda_k y miu_k, y llevamos el contador de iteraciones'
-            lambda_k =  a + (1-(self.fibonacci(n-1)/self.fibonacci(n)))*(b-a) 
+            lambda_k =  a + (1-(self.fibonacci(n-1)/self.fibonacci(n)))*(b-a)
             miu_k    =  a + (self.fibonacci(n-1)/self.fibonacci(n))*(b-a)
             cont    +=  1
-            
+
             'si f(lambda_k) > f(miu_k) ajustamos el valor del intervalo a [lambda_k, b_k]'
-            if self.U(lambda_k) > self.U(miu_k): 
+            if self.U(lambda_k) > self.U(miu_k):
                 # print("It: {:02d} - a_k: {:.10f} - b_k: {:.10f}  - l_k {:.10f}  - u_k{:.10f}  - f(l_k) {:.10f}  - f(u_k) {:.10f} - b_k-a_k {:.10f}".format(cont, a, b, lambda_k, miu_k, U(lambda_k), U(miu_k), b-a))
                 a = lambda_k
-                
+
                 'si f(lambda_k) <= f(miu_k) ajustamos el valor del intervalo a [a_k, miu_k]'
             else:
                 # print("It: {:02d} - a_k: {:.10f} - b_k: {:.10f}  - l_k {:.10f}  - u_k{:.10f}  - f(l_k) {:.10f}  - f(u_k) {:.10f} - b_k-a_k {:.10f}".format(cont, a, b, lambda_k, miu_k, U(lambda_k), U(miu_k), b-a))
                 b = miu_k
-            
+
             print("It: {:02d} - a_k: {:.10f} - b_k: {:.10f}".format(cont, a, b))
 
 
-            ## condicional para cuando llegamos a la iteracion n-2 
+            ## condicional para cuando llegamos a la iteracion n-2
             if cont == n-2: break
-                
-                
+
+
         miu_k = lambda_k + e
-        
+
         '''
         Mostramos el intervalo final luego de que este cumpla con el maximo
         de distancia entre los puntos a y b sea menos a h
         '''
-        
-        if self.U(lambda_k) > self.U(miu_k): 
+
+        if self.U(lambda_k) > self.U(miu_k):
             # print("It: {:02d} - a_k: {:.10f} - b_k: {:.10f}  - l_k {:.10f}  - u_k{:.10f}  - f(l_k) {:.10f}  - f(u_k) {:.10f} - b_k-a_k {:.10f}".format(cont, a, b, lambda_k, miu_k, U(lambda_k), U(miu_k), b-a))
             a = lambda_k
-            
-            "'si f'(lambda_k) <= f'(miu_k) ajustamos el valor del intervalo a [a_k, miu_k]"
+
+            "si f'(lambda_k) <= f'(miu_k) ajustamos el valor del intervalo a [a_k, miu_k]"
         else:
             # print("It: {:02d} - a_k: {:.10f} - b_k: {:.10f}  - l_k {:.10f}  - u_k{:.10f}  - f(l_k) {:.10f}  - f(u_k) {:.10f} - b_k-a_k {:.10f}".format(cont, a, b, lambda_k, miu_k, U(lambda_k), U(miu_k), b-a))
             b = miu_k
-        
-        
+
+
         print("-------------------------------------------------------")
         '''
         Como la solucion optima pertenece al intervalo [a,b]
@@ -286,15 +309,39 @@ class OptimizerManager:
         '''
         print("(a_k + b_k)/2 == {}".format((a + b)/2))
 
-            
 
 
 
+'''
+Notas:
+
+Algunos ejemplos con los que se pueden probar para seccion dorada y fibonacci use
+Funcion: T**4 + 3*(T**3) + T**2 + 2*T + 4
+limites: -4, 2
+
+Nota: Para estos dos metodos no necesitamos definir correctamente derU ni der2U porque
+estos metodos no las usan
+
+
+Para biseccion use
+Funcion: T**4 - T**2 -4*T
+Primera derivada: 4*(T**3) -2*T - 4
+limites: -4, 4
+
+Nota: Para este metodo solo es necesario la funcion y la primera derivada
+
+
+Para newton considere usar:
+Funcion: 3*(T**4) + 4*(T**3)
+Primera derivada: 12*(T**3) + 12*(T**2)
+Segunda derivada: 36*(T**2) + 24*T
+con lambda = -2 o lambda = 2
+'''
 
 
 # Crear una instancia de la clase OptimizerManager y registra los metodos de optimizacion
 optimizer_manager = OptimizerManager()
-optimizer_manager.register_optimizer("GoldenSectionSearch", optimizer_manager.GoldenSectionSearch)
+optimizer_manager.register_optimizer("GoldenSection", optimizer_manager.GoldenSectionSearch)
 optimizer_manager.register_optimizer("Newton", optimizer_manager.newtonSearch)
 optimizer_manager.register_optimizer("Bisection", optimizer_manager.bisectionSearch)
 optimizer_manager.register_optimizer("Fibonacci", optimizer_manager.fibonacciSearch)
@@ -302,9 +349,9 @@ optimizer_manager.register_optimizer("Fibonacci", optimizer_manager.fibonacciSea
 
 
 
+## Definimos el metodo que vamos a usar
+optimizer_manager.algorithm = "GoldenSection"
 
-optimizer_manager.algorithm = "Fibonacci"
-
-# Utilizar el algoritmo de optimización y el gráfico
+# Utilizamos el metodo de optimización y el gráfico
 optimizer_manager.optimize(optimizer_manager.algorithm)
 optimizer_manager.GraphObjectiveFunction()
